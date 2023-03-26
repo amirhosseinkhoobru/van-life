dbe2e4fd13e45b6d04ca1221b2df3ea78ddf3416
amirhosseinkhoobru <120305376+amirhosseinkhoobru@users.noreply.github.com>
2023-03-26 15:59:45 +0430

    Adding files

---
+++ react-router/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+function App() {
+  return (
+    <>
+      <header>
+        <Link className="site-logo" to={"/"}>
+          #VANLIFE
+        </Link>
+        <nav>
+          <Link to={"/about"}>About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
